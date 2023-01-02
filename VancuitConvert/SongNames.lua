1a6a8258c7f22a0420ff1e0eae30fc6cc2006837
Gelatek <76650942+Gelatekussy@users.noreply.github.com>
2023-01-02 10:19:08 -0800

    Create SongNames.lua

---
+++ VancuitConvert/SongNames.lua
@@ -0,0 +1,19 @@
+return {
+	["4476249256"]="OurPsych-Ureshii",
+	["6078491339"]="TRAELMYX - EXP | Complextro",
+	["5640306218"]="cYsmix - Escapism",
+	["4102155984"]="PSYQUI - Education",
+	["5362030098"]="PSYQUI - Stepper Future Core",
+	["5921747599"]="Metaroom - JITTER FANTA",
+	["4102169069"]="PSYQUI - Start Up",
+	["6140681907"]="METAROOM & TELEMIST - DOG BUS",
+	["4660743645"]="PSYQUI - Funk Assembly",
+	["5944081580"]="Sewer - Lexapro Delirium (metaroom remix)",
+	["1113149887"]="RQ - CMonolith - Mega",
+	["2576808051"]="PSYQUI - Hype",
+	["6065535768"]="Sewer - Psychosis",
+	["6108653773"]="METAROOM - OUT OF CONTROL",
+	["6012657271"]="METAROOM - ICON KILLER",
+	["3450285734"]="PSYQUI - Hype (lapix remix)",
+	["511859487"]="Laszlo - Sphere"
+}
